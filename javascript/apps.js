fb2d9238a64d0ae6d81e2682f7733f9bd8674413
SaedSohyoun <9011518@student.zadkine.nl>
2023-10-05 11:21:56 +0200

    sa

---
+++ javascript/apps.js
@@ -0,0 +1,9 @@
+function display(){
+    document.getElementById('title').style.display = 'none';
+    document.getElementById('title').innerText = 'gone';
+}
+
+function changeFont(){
+    document.getElementById('title').style.fontSize = '50px';
+}
+
