6ff77d987b5772cebc1f068ef558f4eea82ed78b
SaedSohyoun <9011518@student.zadkine.nl>
2023-10-06 14:33:09 +0200

    s

---
+++ java6.4/app.js
@@ -0,0 +1,23 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+document.getElementById('demo').innerHTML = myFunction(4, 3);
+
+function calculate(num, num2){
+    console.log(num + num2)
+}
+
+calculate(7, 15)
+
+function multiplyByFive(num){
+    console.log(num * 5)
+}
+
+multiplyByFive(5)
+
+function calculateMinutes(num){
+    console.log(num / 60)
+}
+
+calculateMinutes(3600)
